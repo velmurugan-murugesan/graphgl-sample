f6818d33ab4d47579e1e85da0450e31627514580
velmurugan <velmurugan.murugesan@gmail.com>
2023-07-21 11:41:52 +0530

    added college query and mutations

--- resolvers.js
+++ resolvers.js
@@ -8,10 +8,23 @@ const Query = {
 
  const Mutation = {
     createStudent:(root,args,context,info) => {
-        return db.students.create({collegeId:args.collegeId,
+        const id = db.students.create({collegeId:args.collegeId,
         firstName:args.firstName,
         lastName:args.lastName})
+        return id
+     },
+
+     createCollege:(root, args, context, info) => {
+        const id = db.colleges.create({collegeName:args.collegeName,
+            location:args.location})
+        return db.colleges.get(id)
      }
  }
 
- module.exports = {Query, Mutation}
+ const Student = {
+    college:(root) => {
+        return db.colleges.get(root.collegeId)
+    }
+ }
+
+ module.exports = {Query, Student, Mutation}
